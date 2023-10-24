a878c3e6fc5cbf7e108c591a45be66220718de7a
Mateo <mateo.nicoloff25@gmail.com>
2023-10-24 20:15:52 -0300

    Verificar inputs

--- script.js
+++ script.js
@@ -1,59 +1,110 @@
+/* Contenedores */
 let formulario = document.getElementById("contenedor__formulario");
 let gracias = document.getElementById("contenedor__gracias");
 
+/* Inputs */
 let numeroTarjetaInput = document.getElementById("numero__input");
-let numeroTarjetaFrente = document.getElementById("tarjetaNumero");
 let nombreTarjetaInput = document.getElementById("nombre__input");
-let nombreTarjetaFrente = document.getElementById("tarjetaNombre");
 let mesTarjetaInput = document.getElementById("mes__input");
-let mesTarjetaFrente = document.getElementById("tarjetaMes");
 let anioTarjetaInput = document.getElementById("anio__input");
-let anioTarjetaFrente = document.getElementById("tarjetaAnio");
 let cvcTarjetaInput = document.getElementById("cvc__input");
-let cvcTarjetaAtras = document.getElementById("tarjetaCvc")
+
+/* Datos Tarjetas */
+let numeroTarjetaFrente = document.getElementById("tarjetaNumero");
+let nombreTarjetaFrente = document.getElementById("tarjetaNombre");
+let mesTarjetaFrente = document.getElementById("tarjetaMes");
+let anioTarjetaFrente = document.getElementById("tarjetaAnio");
+let cvcTarjetaAtras = document.getElementById("tarjetaCvc");
+
+/* Errores */
+let mensajeErrorNombre = document.getElementById("nombreError");
+let mensajeErrorNumero = document.getElementById("numeroError");
+let mensajeErrorMes = document.getElementById("mesError");
+let mensajeErrorAnio = document.getElementById("anioError");
+let mensajeErrorCvc = document.getElementById("cvcError");
+
 
 /* Cambiar valores de las tarjetas */
-numeroTarjetaInput.oninput = function (event){   /* capturo lo que se escribe en el input */
-    numeroTarjetaFrente.innerHTML = event.target.value;
-    /* Aca hace referencia al span*/   /* Aca hace referencia al valor del input */
+
+numeroTarjetaInput.oninput = function (event) {
+  /* capturo lo que se escribe en el input */
+  numeroTarjetaFrente.innerHTML =
+    event.target.value; /* Aca hace referencia al valor del input */
+  /* Aca hace referencia al span*/
+};
+
+nombreTarjetaInput.oninput = function (event) {
+  nombreTarjetaFrente.innerHTML = event.target.value;
+};
+
+mesTarjetaInput.oninput = function (event) {
+  mesTarjetaFrente.innerHTML = event.target.value;
+};
+
+anioTarjetaInput.oninput = function (event) {
+  anioTarjetaFrente.innerHTML = event.target.value;
+};
+
+cvcTarjetaInput.oninput = function (event) {
+  cvcTarjetaAtras.innerHTML = event.target.value;
+};
+
+/* Funciones verificar datos */
+
+/* Errores Generales */
+function verificarVacio(atributo, error) {
+  if (atributo.value.length == 0) {
+    error.innerHTML = "Can´t be blank";
+    atributo.style.borderColor = "red";
+    return true;
+  } else {
+    error.innerHTML = "";
+    atributo.style.borderColor = "";
+    return false;
+  }
 }
 
-nombreTarjetaInput.oninput = function (event){
-    nombreTarjetaFrente.innerHTML = event.target.value;
+/* Errores Numero */
+
+function erroresNumero(numero, error) {
+  if (numero.value.length == 0) {
+    error.innerHTML = "Can´t be blank";
+    numero.style.borderColor = "red";
+    return true;
+  } else if (numero.value.length != 16) {
+    error.innerHTML = "Tiene que tener 16 caracteres";
+    numero.style.borderColor = "red";
+    return true;
+  } else if (numero.value.length != 16) {
+    error.innerHTML = "Tiene que tener 16 caracteres";
+    numero.style.borderColor = "red";
+    return true;
+  } else if (isNaN(numero.value)) {
+    error.innerHTML = "Solo acepta numeros";
+    numero.style.borderColor = "red";
+    return true;
+  } else {
+    error.innerHTML = "";
+    numero.style.borderColor = "";
+    return false;
+  }
 }
 
-mesTarjetaInput.oninput = function (event){
-    mesTarjetaFrente.innerHTML = event.target.value;
-}
-
-anioTarjetaInput.oninput = function (event){
-    anioTarjetaFrente.innerHTML = event.target.value;
-}
-
-cvcTarjetaInput.oninput = function (event){
-    cvcTarjetaAtras.innerHTML = event.target.value;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function mostrarGracias(){
+/* Cambiar pagina */
+function mostrarGracias() {
+    
+    let errorNombre = verificarVacio(nombreTarjetaInput, mensajeErrorNombre);
+    let errorMes = verificarVacio(mesTarjetaInput, mensajeErrorMes);
+    let errorAnio = verificarVacio(anioTarjetaInput, mensajeErrorAnio);
+    let errorCvc = verificarVacio(cvcTarjetaInput, mensajeErrorCvc);
+    let errorNumero = erroresNumero(numeroTarjetaInput, mensajeErrorNumero);
+   if(errorNombre == false && errorNumero == false && errorMes == false && errorAnio == false && errorCvc == false){
     gracias.style.display="flex";
     formulario.style.display="none";
+   }
 }
 
-function mostrarFormulario(){
-    formulario.style.display="flex";
-    gracias.style.display="none"; 
+function mostrarFormulario() {
+  formulario.style.display = "flex";
+  gracias.style.display = "none";
 }
-
